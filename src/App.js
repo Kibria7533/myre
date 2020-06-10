dc725a8d0542f920d43c62b25b7fdb0cbcdee45a
Cse_academia <62127870+CSEacademia@users.noreply.github.com>
2020-06-10 14:57:53 +0600

    edit

--- src/App.js
+++ src/App.js
@@ -16,7 +16,7 @@ function App() {
           target="_blank"
           rel="noopener noreferrer"
         >
-          Learn React
+          Learn React app free hosting on heroku
         </a>
       </header>
     </div>
